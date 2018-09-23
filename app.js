d158f8d32747f5cdc36bf79ff8dc08dea39d4c8a
Michael Frieze <michaelfrieze@icloud.com>
2018-09-23 17:53:41 -0400

    just created a simple route

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router'); // have to add the require statement here
+const json = require('koa-json');
 
 const app = new Koa();
+const router = new KoaRouter(); // had to create a new router instance and reference it
 
-// Using async here! pretty cool.
-// ctx is interesting here
-app.use(async ctx => (ctx.body = 'Hello World'));
+// This is just prettier JSON that might do some other stuff
+app.use(json());
 
-// Just starting up a simple server
+// Simple Middleware Example
+// app.use(async ctx => (ctx.body = { msg: 'Hello World' }));
+
+// This is just a simple route at localhost:3000/test
+router.get('/test', ctx => (ctx.body = 'Hello Test'));
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods()); // Now we can use routes
+
 app.listen(3000, () => console.log('Server Started...'));
