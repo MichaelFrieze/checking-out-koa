83b6d8bd5493b32c1700719c7fe9bcf0428456ab
Michael Frieze <michaelfrieze@icloud.com>
2018-09-23 21:59:13 -0400

    Updated this app so we can actually add things to the array from the webpage and it gets updated on the index

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const json = require('koa-json');
-const path = require('path'); // this allows us to work with file paths
-const render = require('koa-ejs'); // had to require the ejs template engine
+const path = require('path');
+const render = require('koa-ejs');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new KoaRouter();
 
+// Just an array we made to replicate a DB or something
+const things = ['My Family', 'Programming', 'Music'];
+
+// Middleware here
 app.use(json());
+app.use(bodyParser());
 
 render(app, {
   root: path.join(__dirname, 'views'),
@@ -17,12 +23,34 @@ render(app, {
   debug: false
 });
 
-router.get('/', async ctx => {
-  await ctx.render('index');
-});
+// Routes
+router.get('/', index);
+router.get('/add', showAdd);
+router.post('/add', add);
 
+// Methods for Routes
+async function index(ctx) {
+  await ctx.render('index', {
+    title: 'Things I love:',
+    thingsView: things
+  });
+}
+
+async function showAdd(ctx) {
+  await ctx.render('add');
+}
+
+async function add(ctx) {
+  const body = ctx.request.body;
+  things.push(body.thing);
+  ctx.redirect('/');
+}
+
+// Test Route
 router.get('/test', ctx => (ctx.body = 'Hello Test'));
 
-app.use(router.routes()).use(router.allowedMethods()); // Now we can use routes
+// I think this has to be here just to use the router
+app.use(router.routes()).use(router.allowedMethods());
 
+// Start the Server
 app.listen(3000, () => console.log('Server Started...'));
